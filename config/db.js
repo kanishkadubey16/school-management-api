0703669fe597302ab23faada651af5159dc9534e
kanishkadubey16 <kanishkadubey16@gmail.com>
2026-05-14 13:53:13 +0530

    Configured Aiven MySQL and deployment setup

--- config/db.js
+++ config/db.js
@@ -1,12 +1,19 @@
 const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-// Create a connection pool instead of a single connection
+// Create MySQL connection pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "school_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+
+  // Required for Aiven cloud MySQL
+  ssl: {
+    rejectUnauthorized: false,
+  },
+
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
